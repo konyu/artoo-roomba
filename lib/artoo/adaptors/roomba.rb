b099e1bea86478e16e919a1f08f8918c1b1d4c4d
Yusuke KON <yusukedrive@gmail.com>
2015-05-18 23:56:00 +0900

    aa

--- lib/artoo/adaptors/roomba.rb
+++ lib/artoo/adaptors/roomba.rb
@@ -9,9 +9,11 @@ module Artoo
 
       # Creates serial or tcp connection
       # @return [Boolean]
-      def connect
+      def connect(baud: 115200)
+        p baud
+        p "========================="
         if port.is_serial?
-          @sp = connect_to_serial(speed = 115200)
+          @sp = connect_to_serial(speed = baud)
           @sp.dtr = 0
           @sp.rts = 0
         else
